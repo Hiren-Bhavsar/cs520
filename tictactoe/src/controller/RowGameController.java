b23ad448e9ff39c7cfc16064606fc0eb8ead1e08
Hiren-Bhavsar <bhavsar.hsb@gmail.com>
2022-04-04 17:28:10 -0400

    Created a checkWin method
    Separated out checking win conditions away from the move method

--- tictactoe/src/controller/RowGameController.java
+++ tictactoe/src/controller/RowGameController.java
@@ -7,12 +7,16 @@ import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
 
+import model.RowBlockModel;
 import model.RowGameModel;
 import view.RowGameGUI;
 
 public class RowGameController {
     public RowGameModel gameModel;
     public RowGameGUI gameView;
+
+	private String playerTwoWinPhrase = "Player 2 wins!";
+	private String playerOneWinPhrase = "Player 1 wins!";
 
     /**
      * Creates a new game initializing the GUI.
@@ -402,6 +406,72 @@ public class RowGameController {
 	    }
 	}
     }
+
+	/**
+	 * Checks to see if anyone has won the game.
+	 */
+
+	 public void checkWin(){
+		 //Check Rows and Columns
+		 for(int x = 0; x < 3; x++){
+			 if(!gameModel.blocksData[x][0].getContents().equals("")){
+				String temp0 = gameModel.blocksData[x][0].getContents();
+				String temp1 = gameModel.blocksData[x][1].getContents();
+				String temp2 = gameModel.blocksData[x][2].getContents();
+				if(temp0.equals(temp1) && temp0.equals(temp2)){
+					endGame();
+					gameModel.setFinalResult(getWinningPlayer(center));
+				}
+			 }
+			 if(!gameModel.blocksData[0][x].getContents().equals("")){
+				String temp0 = gameModel.blocksData[0][x].getContents();
+				String temp1 = gameModel.blocksData[1][x].getContents();
+				String temp2 = gameModel.blocksData[2][x].getContents();
+				if(temp0.equals(temp1) && temp0.equals(temp2)){
+					endGame();
+					gameModel.setFinalResult(getWinningPlayer(center));
+				}
+			 }
+		 }
+
+		 //Check Diagonals
+		 String udl = gameModel.blocksData[0][0].getContents();
+		 String udr = gameModel.blocksData[0][2].getContents();
+		 String ldl = gameModel.blocksData[2][0].getContents();
+		 String ldr = gameModel.blocksData[2][2].getContents();
+		 String center = gameModel.blocksData[1][1].getContents();
+
+		 if(!center.equals("")){
+			if(udl.equals(center) && ldr.equals(ldr)){
+				endGame();
+				gameModel.setFinalResult(getWinningPlayer(center));
+			}else if(udr.equals(center) && ldl.equals(ldr)){
+				endGame();
+				gameModel.setFinalResult(getWinningPlayer(center));
+			}
+		}
+
+		//Check for a Tie
+		String toCheck = "";
+		for(RowBlockModel r : gameModel.blocksData){
+			toCheck += r.getContents();
+		}
+
+		if(toCheck.length() == 0){
+			endGame();
+			gameModel.setFinalResult(RowGameModel.GAME_END_NOWINNER);
+		}
+	 }
+
+	 private String getWinningPlayer(String s){
+		if(s.toLowerCase().equals("o")){
+			return playerTwoWinPhrase;
+		}else{
+			return playerOneWinPhrase;
+		}
+	 }
+
+
 
     /**
      * Ends the game disallowing further player turns.
